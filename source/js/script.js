dab109a8b8049f3dcdc5fa736a301186f5c9e186
Andrey Fedinyak <fedinyakandrey@gmail.com>
2020-05-17 22:13:17 +0300

    make picturefill

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,9 @@
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
+var header = document.querySelector(".page-header");
 
 navMain.classList.remove("main-nav--nojs");
+header.classList.remove("page-header--nojs");
 
 navToggle.addEventListener("click", function() {
   if (navMain.classList.contains("main-nav--closed")) {
